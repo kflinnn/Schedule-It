87aee4fdcd2342807025bcf5f31b8d90f01b9807
kflinnn <kflinncoding@gmail.com>
2022-12-12 15:20:15 -0500

    created functions for all buttons so that all information is saved in local storage

--- Assets/script.js
+++ Assets/script.js
@@ -11,14 +11,69 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  
-  var saveButton = $("button");
-  saveButton.on("click", function() {
+  //First Save Button
+  var saveButtonNine = $("#hour-9, button");
+  saveButtonNine.on("click", function() {
     var enteredHourNine = document.querySelector("#hour-9-text").value;
     localStorage.setItem("hour-9", enteredHourNine);
     
     //readTasksFromStorage();
   })
+//Second Save Button
+  var saveButtonTen = $("#hour-10, button");
+  saveButtonTen.on("click", function(){
+    var enteredHourTen = document.querySelector("#hour-10-text").value;
+    localStorage.setItem("hour-10", enteredHourTen);
+  })
+
+//Third Save Button
+var saveButtonEleven = $("#hour-11, button");
+saveButtonEleven.on("click", function(){
+  var enteredHourEleven = document.querySelector("#hour-11-text").value;
+  localStorage.setItem("hour-11", enteredHourEleven);
+})
+
+//Fourth Save Button
+var saveButtonTwelve = $("#hour-12, button");
+saveButtonTwelve.on("click", function(){
+  var enteredHourTwelve = document.querySelector("#hour-12-text").value;
+  localStorage.setItem("hour-12", enteredHourTwelve);
+})
+
+//Fifth Save Button
+var saveButtonOne = $("#hour-1, button");
+saveButtonOne.on("click", function(){
+  var enteredHourOne = document.querySelector("#hour-1-text").value;
+  localStorage.setItem("hour-1", enteredHourOne);
+})
+
+//Sixth Save Button
+var saveButtonTwo = $("#hour-2, button");
+saveButtonTwo.on("click", function(){
+  var enteredHourTwo = document.querySelector("#hour-2-text").value;
+  localStorage.setItem("hour-2", enteredHourTwo);
+})
+
+//Seventh Save Button
+var saveButtonThree = $("#hour-3, button");
+saveButtonThree.on("click", function(){
+  var enteredHourThree = document.querySelector("#hour-3-text").value;
+  localStorage.setItem("hour-3", enteredHourThree);
+})
+
+//Eighth Save Button
+var saveButtonFour = $("#hour-4, button");
+saveButtonFour.on("click", function(){
+  var enteredHourFour = document.querySelector("#hour-4-text").value;
+  localStorage.setItem("hour-4", enteredHourFour);
+})
+
+//Ninth Save Button
+var saveButtonFive = $("#hour-5, button");
+saveButtonFive.on("click", function(){
+  var enteredHourFive = document.querySelector("#hour-5-text").value;
+  localStorage.setItem("hour-5", enteredHourFive);
+})
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -26,6 +81,8 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
+
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
